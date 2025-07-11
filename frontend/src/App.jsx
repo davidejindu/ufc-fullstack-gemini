cc7d6cded2609f490b50cf91f26e6da962c9a7b8
David Ejindu <ejindudavid540@gmail.com>
2025-07-11 01:01:34 -0400

    tailwind check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
+import React from 'react'
 import Header from './components/Header'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Header/>
-
-      
-    </>
+    <div className="min-h-screen bg-red-500">
+    <div className="min-h-screen bg-red-500 text-white p-10 border-4 border-blue-500">
+  <h1 className="text-4xl font-bold text-center">Tailwind is Working</h1>
+</div>
+    </div>
   )
 }
 
